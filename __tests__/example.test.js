c3f3ecfbe77bc29b3f4b06dceba7001042120522
Raj Kumar Panda <rajkumar@stepsecurity.io>
2025-02-14 01:41:26 +0530

    ignoring test vulnerabilities

--- __tests__/example.test.js
+++ __tests__/example.test.js
@@ -7,3 +7,7 @@ test('adds 1 + 2 to equal 3', () => {
 test('adds 2 + 2 to equal 4', () => {
     expect(2 + 2).toBe(4);
   });
+
+  test('adds 2 + 2 to equal 5', () => {
+    expect(2 + 3).toBe(5);
+  });
