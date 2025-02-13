675e838e964a8f4eedf28dfb9e77fdf0df73c87f
Raj Kumar Panda <rajkumar@stepsecurity.io>
2025-02-14 01:45:52 +0530

    j

--- __tests__/example.test.js
+++ __tests__/example.test.js
@@ -8,7 +8,7 @@ test('adds 2 + 2 to equal 4', () => {
     expect(2 + 2).toBe(4);
   });
   
-test('adds 2 + 2 to equal 4', () => {
+test('adds 2 + 2 to equal 5', () => {
     expect(2 + 3).toBe(5);
   });
   
